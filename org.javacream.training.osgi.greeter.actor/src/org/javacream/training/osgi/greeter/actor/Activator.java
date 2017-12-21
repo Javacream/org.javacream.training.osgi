74705224f2e521583122eea52024390495b68ed7
Rainer Sawitzki <training@rainer-sawitzki.de>
2017-12-21 09:33:57 +0100

    add version example

--- org.javacream.training.osgi.greeter.actor/src/org/javacream/training/osgi/greeter/actor/Activator.java
+++ org.javacream.training.osgi.greeter.actor/src/org/javacream/training/osgi/greeter/actor/Activator.java
@@ -1,17 +1,27 @@
 package org.javacream.training.osgi.greeter.actor;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import org.javacream.training.osgi.greeter.SimpleGreeter;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
 
+	private ScheduledExecutorService scheduledExecutorService;
+
 	public void start(BundleContext bundleContext) throws Exception {
+		scheduledExecutorService = Executors.newScheduledThreadPool(1);
 		SimpleGreeter simpleGreeter = new SimpleGreeter();
-		System.out.println(simpleGreeter.greet("Hugo"));
+		scheduledExecutorService.scheduleAtFixedRate( () ->
+				System.out.println(simpleGreeter.greet("Hugo"))
+		, 0, 5, TimeUnit.SECONDS);
 	}
 
 	public void stop(BundleContext bundleContext) throws Exception {
+		scheduledExecutorService.shutdown();
 	}
 
 }
