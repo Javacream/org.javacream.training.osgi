e841bde42b89059590c3e617cf7f6b79412ec0fc
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-07-03 14:30:43 +0200

    add ds example

--- org.javacream.storeservice.actor/src/org/javacream/storeservice/actor/StoreServiceActor.java
+++ org.javacream.storeservice.actor/src/org/javacream/storeservice/actor/StoreServiceActor.java
@@ -1,34 +1,17 @@
 package org.javacream.storeservice.actor;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
 import org.javacream.storeservice.api.StoreService;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
+@Component(immediate = true)
 public class StoreServiceActor {
 
-	private StoreService storeService;
-	public void setStoreService(StoreService storeService) {
-		this.storeService = storeService;
-	}
-
-	private ScheduledExecutorService executorService;
+	@Reference private StoreService storeService;
 	
-	
-	public void startup(){
-		executorService = Executors.newScheduledThreadPool(1);
-		executorService.scheduleAtFixedRate(new Runnable(){
-
-			@Override
-			public void run() {
-				System.out.println(storeService.getStock("ITEM-ID"));
-			}
-			
-		}, 0, 5, TimeUnit.SECONDS);
-	}
-	
-	public void finish(){
-		executorService.shutdown();
+	@Activate public void startUp() {
+		System.out.println(storeService);
+		System.out.println(storeService.getStock("ISBN1"));
 	}
 }
--- org.javacream.storeservice.impl/src/org/javacream/storeservice/impl/BooksStoreService.java
+++ org.javacream.storeservice.impl/src/org/javacream/storeservice/impl/BooksStoreService.java
@@ -1,10 +1,18 @@
 package org.javacream.storeservice.impl;
 
-import org.javacream.storeservice.api.StoreService;
 
+import org.javacream.storeservice.api.StoreService;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+@Component
 public class BooksStoreService implements StoreService{
 	private int stock = 42;
 
+	@Activate 
+	public void init() {
+		System.out.println("Initializing " + this);
+	}
 	@Override
 	public int getStock(String itemId){
 		System.out.println("retrieving stock for isbn " + itemId);
